f20f1f0a345ca11c66801863c1529f253c048109
Bala Sutherson <36947407+BalaSubramanianSutherson@users.noreply.github.com>
2024-01-09 23:02:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const auth = require('./middlewares/auth')
+
+
+app.use(cors());
+app.use(express.json());
+app.use(auth)
+
+const errorHandler = (err, req, res, next) =>{
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+}
+app.use(errorHandler)
+
+const userRouter = require('./routes/user_router');
+const mylistRouter = require('./routes/mylist_router');
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my server!');
+  });
+
+app.use('/user', userRouter)
+app.use('/mylist', mylistRouter)
+
+app.listen(3001, () => {
+    console.log("Yey, your server is running on port 3001");
+  });
