c7d417a9450b07f28831cbee39725c1a6f41b1fb
Bala Sutherson <36947407+BalaSubramanianSutherson@users.noreply.github.com>
2024-01-17 14:28:31 +0530

    Added content controller

---
+++ controllers/content_controller.js
@@ -0,0 +1,147 @@
+const fs = require('fs');
+const axios = require('axios');
+const cron = require('node-cron');
+
+async function getContents(req, res, next){
+    console.log('Get Contents request'+ req.query.title)
+    if(req.query.title){
+        const storedMovieData = JSON.parse(fs.readFileSync('movies.json'));
+        const movies = storedMovieData.filter(node => node.title && node.title.toLowerCase().includes(req.query.title.toLowerCase()));
+        const storedSeriesData = JSON.parse(fs.readFileSync('series.json'));
+        const series = storedSeriesData.filter(node => node.title && node.title.toLowerCase().includes(req.query.title.toLowerCase()));
+        const storedStreamData = JSON.parse(fs.readFileSync('livestream.json'));
+        const stream = storedStreamData.filter(node => node.name && node.name.toLowerCase().includes(req.query.title.toLowerCase()));
+        const response = { "movies": movies,
+                        "series": series,
+                        "livestream": stream
+                    }
+        res.send(response)
+    }else{
+        res.send("-1")
+    }
+}
+
+async function getMovieContents(){
+    console.log('Movie contents start updating '+ new Date());
+    axios.get('http://ky-iptv.com/player_api.php?username=walkerrodney216%40gmail.com&password=Stream1958&action=get_vod_streams')
+    .then(response => {
+
+        const contents = response.data;
+        const desiredAttributes = ['name', 'title', 'stream_id', 'stream_icon'];
+        console.log("Movie contents size "+ contents.length)
+        // Extract and create new objects with only those attributes
+        const filteredData = contents.map(node => {
+        return desiredAttributes.reduce((obj, key) => {
+            obj[key] = node[key];
+            return obj;
+        }, {});
+        });
+
+        // Write the filtered data to a file
+        fs.writeFileSync('movies.json', JSON.stringify(filteredData, null, 2));
+        console.log('Movie contents updated '+ new Date());
+        
+      })
+      .catch(error => {
+        console.error('Error fetching Movie data:', error);
+        throw error;
+      });
+}
+
+async function updateMovieContents(req, res, next){
+    try{
+        getMovieContents()
+        res.send('updated') 
+    }catch(err){
+        console.error('updateMovieContents function error:', err);
+        next(err)
+    }
+}
+
+async function getSeriesContents(){
+    console.log('Series contents start updating '+ new Date());
+    axios.get('http://ky-iptv.com/player_api.php?username=walkerrodney216%40gmail.com&password=Stream1958&action=get_series')
+    .then(response => {
+        const contents = response.data;
+        const desiredAttributes = ['name', 'title', 'series_id', 'cover'];
+        console.log("Series contents size "+ contents.length)
+        // Extract and create new objects with only those attributes
+        const filteredData = contents.map(node => {
+        return desiredAttributes.reduce((obj, key) => {
+            obj[key] = node[key];
+            return obj;
+        }, {});
+        });
+
+        // Write the filtered data to a file
+        fs.writeFileSync('series.json', JSON.stringify(filteredData, null, 2));
+        console.log('Series contents updated '+ new Date());
+        
+      })
+      .catch(error => {
+        console.error('Error fetching Series data:', error);
+        throw error;
+      });
+}
+
+async function updateSeriesContents(req, res, next){
+    try{
+        getSeriesContents()
+        res.send('updated series content') 
+    }catch(err){
+        console.error('updateSeriesContents function error:', err);
+        next(err)
+    }
+}
+
+async function getLiveStreamContents(){
+    console.log('LiveStream contents start updating '+ new Date());
+    axios.get('http://ky-iptv.com/player_api.php?username=walkerrodney216%40gmail.com&password=Stream1958&action=get_live_streams')
+    .then(response => {
+        const contents = response.data;
+        const desiredAttributes = ['name', 'stream_id', 'stream_icon'];
+        console.log("Series contents size "+ contents.length)
+        // Extract and create new objects with only those attributes
+        const filteredData = contents.map(node => {
+        return desiredAttributes.reduce((obj, key) => {
+            obj[key] = node[key];
+            return obj;
+        }, {});
+        });
+
+        // Write the filtered data to a file
+        fs.writeFileSync('livestream.json', JSON.stringify(filteredData, null, 2));
+        console.log('Live stream contents updated '+ new Date());
+    
+      })
+      .catch(error => {
+        console.error('Error fetching Live stream data:', error);
+        throw error;
+      });
+}
+
+async function updateLiveContents(req, res, next){
+    try{
+        getLiveStreamContents()
+        res.send('updated live contents') 
+    }catch(err){
+        console.error('updateLiveContents function error:', err);
+        next(err)
+    }
+}
+
+
+// Setting a cron job for every min
+cron.schedule('*/2 * * * *', ()=> {
+    console.log('running every minute 1, 2, 4 and 5');
+  });
+cron.schedule('*/2 * * * *', getMovieContents);
+cron.schedule('*/3 * * * *', getSeriesContents);
+cron.schedule('*/4 * * * *', getLiveStreamContents);
+
+module.exports = {
+    getContents,
+    updateMovieContents,
+    updateSeriesContents,
+    updateLiveContents
+  };
