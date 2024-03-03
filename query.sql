b0edc0c56a4fe9328f18e72a2ac9f5b8074c3244
Bala Sutherson <36947407+BalaSubramanianSutherson@users.noreply.github.com>
2024-03-03 21:59:26 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -15,3 +15,13 @@ CREATE TABLE trinityhub.dbo.mylist (
 	CONSTRAINT PK__mylist__AB6E616569A7F7E5 PRIMARY KEY (email),
 	CONSTRAINT FK_userId FOREIGN KEY (email) REFERENCES trinityhub.dbo.users(email)
 );
+
+
+CREATE TABLE trinityhub.dbo.playback (
+	email varchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	content_id varchar(255),
+	position varchar(255),
+	created_at datetime DEFAULT getdate() NULL,
+	CONSTRAINT PK__playback__AB6E616569A7F7E5 PRIMARY KEY (email, content_id),
+	CONSTRAINT FK_playbacl_userId FOREIGN KEY (email) REFERENCES trinityhub.dbo.users(email)
+);
